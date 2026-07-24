aac3528eba8cef736dae292a0ae326eba9211ca9
cyrus maina <cmaina2@my.centennialcollege.ca>
2026-07-24 01:55:53 -0400

    Add missing values and visualization

--- Bicycle_theft_Group3_section_COMP309Project.py
+++ Bicycle_theft_Group3_section_COMP309Project.py
@@ -86,3 +86,164 @@ correlation =data_maina.select_dtypes(include=np.number).corr()
 print(correlation)
 
 print("\n-----------Minimum ")
+minimum=data_maina.select_dtypes(include=np.number).min()
+print(minimum)
+
+print("\n------------------maximum")
+maximum=data_maina.select_dtypes(include=np.number).max()
+print(maximum)
+
+
+#C. MISSING VALUE
+
+print("------------Missing Value Percentages")
+
+missing_per = (data_maina.isnull().sum()/ len(data_maina))*100
+print(missing_per.round(2))
+
+
+print ("\n------------Columns with Missing Values ")
+
+missing_v =data_maina.isnull().sum()
+missing_v = missing_v[missing_v > 0]
+
+print(missing_v)
+
+print ("\n----------negatitive bike cost")
+print((data_maina["BIKE_COST"] < 0).sum())
+
+print("\n-----------latitude")
+print((data_maina["LAT_WGS84"] == 0).sum())
+
+print("\n-----------Longititude")
+print((data_maina["LONG_WGS84"] == 0).sum())
+
+#D GRAPH & VISUALIZATION
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Histogram of bicycle cost
+plt.figure(figsize=(8,5))
+
+plt.hist(data_maina["BIKE_COST"], bins=30)
+
+plt.title("Distribution of Bicycle Costs")
+
+plt.xlabel("Bike Cost ($)")
+
+plt.ylabel("Number of Bicycles")
+
+plt.show()
+
+
+#bicycle type stolen the most(count plot)
+plt.figure(figsize=(10,6))
+
+sns.countplot(
+    data=data_maina,
+    x="BIKE_TYPE"
+)
+
+plt.title("Number of Bicycle stolen the most by Bike Type")
+
+plt.xticks(rotation=45)
+
+plt.show()
+
+#Bicycle theft by year(count plot)
+
+plt.figure(figsize=(8,5))
+
+sns.countplot(
+    data=data_maina,
+    x="REPORT_YEAR"
+)
+
+plt.title("Bicycle Thefts by Report Year")
+
+plt.show()
+
+
+#Top 10 Neighbourhoods with Bicycle Thefts
+
+plt.figure(figsize=(12,6))
+
+top_neighbourhoods = data_maina["NEIGHBOURHOOD_158"].value_counts().head(10)
+
+sns.barplot(
+    x=top_neighbourhoods.values,
+    y=top_neighbourhoods.index
+)
+
+plt.title("Top 10 Neighbourhoods with Bicycle Thefts")
+
+plt.xlabel("Number of Bicycle Thefts")
+
+plt.ylabel("Neighbourhood")
+
+plt.show()
+
+# Correlation Heatmap
+
+
+# Create list of numerical columns to analyze
+corr_columns = [
+    "OCC_YEAR",
+    "OCC_DAY",
+    "OCC_DOY",
+    "OCC_HOUR",
+    "REPORT_YEAR",
+    "REPORT_DAY",
+    "REPORT_DOY",
+    "REPORT_HOUR",
+    "BIKE_SPEED",
+    "BIKE_COST",
+    "LAT_WGS84",
+    "LONG_WGS84"
+]
+
+
+plt.figure(figsize=(12,8))
+
+corr_matrix = data_maina[corr_columns].corr()
+
+sns.heatmap(
+    corr_matrix,
+    annot=True,
+    cmap="coolwarm",
+    fmt=".2f"
+)
+
+plt.title("Correlation Heatmap of Bicycle Theft Data")
+
+plt.show()
+
+#Missing Values Bar Chart
+
+missing_values = data_maina.isnull().sum()
+
+missing_values = missing_values[missing_values > 0]
+
+
+plt.figure(figsize=(10,6))
+
+
+sns.barplot(
+    x=missing_values.values,
+    y=missing_values.index
+)
+
+
+plt.title("Missing Values by Column")
+
+plt.xlabel("Number of Missing Values")
+
+plt.ylabel("Column Name")
+
+
+plt.show()
+
+
+
+
